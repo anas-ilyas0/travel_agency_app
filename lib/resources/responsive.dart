c37bb19b5b36fd2458d54ad0925b93fcad15998d
usman <usmanghanifayyaz2@gmail.com>
2024-10-03 00:49:30 -0700

    usman

--- lib/resources/responsive.dart
+++ lib/resources/responsive.dart
@@ -16,24 +16,24 @@ class Responsive extends StatelessWidget {
 
   // This isMobile, isTablet, isDesktop helep us later
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 600;
+      MediaQuery.of(context).size.width < 850;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1024 &&
-      MediaQuery.of(context).size.width >= 600;
+      MediaQuery.of(context).size.width < 1100 &&
+      MediaQuery.of(context).size.width >= 850;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1024;
+      MediaQuery.of(context).size.width >= 1100;
 
   @override
   Widget build(BuildContext context) {
     final Size _size = MediaQuery.of(context).size;
     // If our width is more than 1100 then we consider it a desktop
-    if (_size.width >= 1024) {
+    if (_size.width >= 1100) {
       return desktop!;
     }
     // If width it less then 1100 and more then 850 we consider it as tablet
-    else if (_size.width >= 600 && tablet != null) {
+    else if (_size.width >= 850 && tablet != null) {
       return tablet!;
     }
     // Or less then that we called it mobile
